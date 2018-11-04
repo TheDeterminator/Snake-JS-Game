70866d4f8ffef852772b2bb98ebcc34787d50263
Alec <afroenygam@gmail.com>
2018-11-04 16:52:46 -0500

    code refactored

--- play_snake.js
+++ play_snake.js
@@ -1,57 +1,57 @@
-let snake
-let food // Variable to hold the food "object" that snake will eat to grow in size
-let res = 20 // The resolution or scaling for the games objects
-let w
-let h
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
 
 function setup() {
-    createCanvas(400, 400)
-    w = floor(width/res)
-    h = floor(height/res)
-    snake = new Snake()
-    frameRate(10)
-    setFoodLocation()
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
 }
 
-function setFoodLocation() {
-    let x = floor(random(w))
-    let y = floor(random(h))
-    food = createVector(x,y)
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+  
 }
 
-function keyPressed() { // Built-in p5 click event that execute Snake method to move Snake object
-    switch(keyCode) {
-	case UP_ARROW:
-	    snake.setDir(0, -1)
-	    break
-	case DOWN_ARROW:
-	    snake.setDir(0, 1)
-	    break
-	case LEFT_ARROW:
-	    snake.setDir(-1, 0)
-	    break
-	case RIGHT_ARROW:
-	    snake.setDir(1,0)
-	    break
-    }
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+  	snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+  	snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+  	snake.setDir(0, -1);
+  } else if (key == ' ') {
+  	snake.grow();
+  }
+
 }
 
 function draw() {
-    scale(res)
-    background(0)
-    if (snake.eat(food)) setFoodLocation()
-    snake.update()
-    snake.show()
-    
-    if (snake.endGame())
-    {
-	print("GAME OVER")
-	background(5, 200, 0)
-	noLoop()
-    }
-
-    noStroke()
-    fill(255, 0, 0)
-    rect(food.x, food.y, 1,1)
+  scale(rez);
+  background(220);
+  if (snake.eat(food)) {
+     foodLocation();
+  }
+  snake.update();
+  snake.show();
+  
+  
+  if (snake.endGame()) {
+  	print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+  
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
 }
-
